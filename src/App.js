f59a7bc18f86f4bef5bf5d27557efa8d11a73548
pradeeprockz <pradeeprockz16@gmail.com>
2023-09-24 13:44:51 +0530

    solution

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from "./Components/UserProfile"
+
+const UserDetails = {
+  imageUrl:"C:\\Users\\hp\\Desktop\\Image(4).jpeg",
+  namme : "pradeep",
+  role: "web designer"
+  
+}
+
+const App = () => <UserProfile/>
+export default App 
---
+++ src/Components/UserProfile/index.js
@@ -0,0 +1,7 @@
+ const UserProfile = (
+    <div>
+        <img src = "C:\\Users\\hp\\Desktop\\Image(4).jpeg"/>
+    </div>
+ )
+
+ export default UserProfile
